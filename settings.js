972d2dd862c346bca70d9958d2b503c958e24a68
DARKOFC009 <84442326+nikhilarma@users.noreply.github.com>
2024-04-21 23:54:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -50,31 +50,31 @@ global.location = process.env.GL_INFO || "Pakistan, Lahore, Sabzazar" //ur locat
 
 //new
 //________________________________________________________________________________________________________
-global.botname = process.env.BOT_NAME || 'XLICON-V3-MD' //ur bot name
+global.botname = process.env.BOT_NAME || '•⏤⵿༄ 𝆺𝅥⃝⃪⃜❤️ᴄ͢͢͢ʀɪᴍɪɴᴀʟ𓆪ꪾ❦︎' //ur bot name
 //________________________________________________________________________________________________________
-global.ownernumber = process.env.SUDO || '923184070915' //ur owner number
+global.ownernumber = process.env.SUDO || '919630863919,91915343616' //ur owner number
 //________________________________________________________________________________________________________
-global.ownername = process.env.SUDO_NAME || 'Salman Ahmad' //ur owner name
+global.ownername = process.env.SUDO_NAME || '•⏤⵿༄ 𝆺𝅥⃝⃪⃜❤️ᴄ͢͢͢ʀɪᴍɪɴᴀʟ𓆪ꪾ❦︎' //ur owner name
 //________________________________________________________________________________________________________
-global.websitex = process.env.YT_LINK || "https://youtube.com/@s4salmanyt"
+global.websitex = process.env.YT_LINK || "https://youtube.com/@DARKOGC009?si=MMq571ft-Q2fE-Yj"
 //________________________________________________________________________________________________________
-global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
+global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0029hhhhjjj"
 //________________________________________________________________________________________________________
 global.themeemoji = process.env.GL_EMOJI || '🏮'
 //________________________________________________________________________________________________________
-global.wm = process.env.GL_WM || "© Xʟɪᴄᴏɴ-Iɴᴄ"
+global.wm = process.env.GL_WM || "•⏤⵿༄ 𝆺𝅥⃝⃪⃜❤️ᴄ͢͢͢ʀɪᴍɪɴᴀʟ𓆪ꪾ❦︎"
 //________________________________________________________________________________________________________
 global.botscript = process.env.GL_SC || 'https://github.com/salmanytofficial/XLICON-V3-MD' //script link
 //________________________________________________________________________________________________________
-global.packname = process.env.PACK_NAME || "𝐗𝐋𝐈𝐂𝐎𝐍-𝐕𝟑"
+global.packname = process.env.PACK_NAME || "•⏤⵿༄ 𝆺𝅥⃝⃪⃜❤️ᴄ͢͢͢ʀɪᴍɪɴᴀʟ𓆪ꪾ❦︎"
 //________________________________________________________________________________________________________
-global.author = process.env.AUTHER_NAME || "ᗰᗩᗪE ᗷY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿"
+global.author = process.env.AUTHER_NAME || "ᗰᗩᗪE ᗷY •⏤⵿༄ 𝆺𝅥⃝⃪⃜❤️ᴄ͢͢͢ʀɪᴍɪɴᴀʟ𓆪ꪾ❦︎"
 //________________________________________________________________________________________________________
-global.creator = process.env.GL_DEVS || "923184070915@s.whatsapp.net"
+global.creator = process.env.GL_DEVS || "919630863919@s.whatsapp.net"
 //________________________________________________________________________________________________________
-global.xprefix = process.env.PREFIX || '.'
+global.xprefix = process.env.PREFIX || '#'
 //________________________________________________________________________________________________________
-global.premium = process.env.GL_VIP || ["923184070915"] // Premium User
+global.premium = process.env.GL_VIP || ["919630863919,91915343616"] // Premium User
 //________________________________________________________________________________________________________
 global.hituet = 0
 //________________________________________________________________________________________________________
